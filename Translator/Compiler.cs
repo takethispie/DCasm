be31a4ce0fe16297bb0147b654d4fb6e622994f8
takethispie <takethispie@gmail.com>
2020-06-20 01:28:06 +0200

    wip: compiler Process function

--- Translator/Compiler.cs
+++ Translator/Compiler.cs
@@ -6,26 +6,28 @@ namespace DCasm.Translator
 {
     public class Compiler
     {
-        public List<string> Program { get; set; }
+        public List<string> Program;
         private InstructionSet isa { get; set; }
-        private Flattener flattener { get; set; }
+        private int pc;
 
         public Compiler(Dictionary<string, Function> functions, bool verbose = false) {
             Program = new List<string>();
             isa = new InstructionSet();
-            flattener = new Flattener(functions);
         }
 
         public void Compile(List<INode> nodes) {
             
             foreach (var node in nodes)
             {
-                Process(node);   
+                var res = Process(node);  
+                pc += res.pcIncr;
+                Program.Add(res.instruction);
             }
         }
 
-        public string Process(INode node) => node switch
+        public (string instruction, int pcIncr) Process(INode node) => node switch
         {
+            Store st => isa.Store(st),
             _ => throw new Exception("Unknown Error")
         };
     }
--- Translator/InstructionSet.cs
+++ Translator/InstructionSet.cs
@@ -5,12 +5,11 @@ namespace DCasm.Translator {
 
     public class InstructionSet {
 
-        public void Store(Store n, ref List<string> program,ref int pc) {
+        public (string instruction, int pcIncr) Store(Store n) {
             var inst = OpCodes.OpToBinary(n.Value) + RegisterConverter.RegisterToBinary(n.BaseRegister) 
             + RegisterConverter.RegisterToBinary(n.OffsetRegister) + RegisterConverter.RegisterToBinary(n.DataValue)
             + "00000000000";
-            program.Add(inst);
-            pc++;
+            return (inst, 1);
         }
 
     }
