569c501c6113bae2e2c2c40effc0d46fab07a7b7
takethispie <takethispie@gmail.com>
2020-06-20 18:30:20 +0200

    remove instruction set

--- Translator/Compiler.cs
+++ Translator/Compiler.cs
@@ -7,12 +7,11 @@ namespace DCasm.Translator
     public class Compiler
     {
         public List<string> Program;
-        private InstructionSet isa { get; set; }
+        private Dictionary<string, int> functionsAdress { get; set; }
         private int pc;
 
         public Compiler(Dictionary<string, Function> functions, bool verbose = false) {
             Program = new List<string>();
-            isa = new InstructionSet();
         }
 
         public void Compile(List<INode> nodes) {
@@ -21,14 +20,29 @@ namespace DCasm.Translator
             {
                 var res = Process(node);  
                 pc += res.pcIncr;
-                Program.Add(res.instruction);
+                Program.AddRange(res.instruction);
             }
         }
 
-        public (string instruction, int pcIncr) Process(INode node) => node switch
+        public (IEnumerable<string> instruction, int pcIncr) Process(INode node) => node switch
         {
-            Store st => isa.Store(st),
+            Store st => Store(st),
             _ => throw new Exception("Unknown Error")
         };
+
+        public (IEnumerable<string> instruction, int pcIncr) Store(Store n) {
+            var inst = OpCodes.OpToBinary(n.Value) + RegisterConverter.RegisterToBinary(n.BaseRegister) 
+            + RegisterConverter.RegisterToBinary(n.OffsetRegister) + RegisterConverter.RegisterToBinary(n.DataValue)
+            + "00000000000";
+            return (new List<string> { inst }, 1);
+        }
+
+        public (IEnumerable<string> instruction, int pcIncr) Call (Call n)
+        {
+            var address = functionsAdress[n.Value];
+            var setInst = OpCodes.OpToBinary("set") + "00111" + "00000" + ConstConverter.ConstantToBinary(address.ToString());
+            var inst = OpCodes.OpToBinary("call") + "00000" + "00111" + ConstConverter.ConstantToBinary("0");
+            return (new List<string> { setInst, inst}, 2);
+        }
     }
 }
